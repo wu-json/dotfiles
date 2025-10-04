6bf102d34a190a7e4b222b27ec36eaf47f185241
wu-json <jason.wu2153@gmail.com>
2025-10-04 12:28:37 -0700

    feat: local lush config

--- .config/nvim/lua/plugins/aesthetics.lua
+++ .config/nvim/lua/plugins/aesthetics.lua
@@ -1,20 +1,7 @@
 return {
   {
-    "EdenEast/nightfox.nvim",
-    opts = {
-      options = {
-        transparent = true,
-      },
-      groups = {
-        all = {
-          NormalFloat = { fg = "fg1", bg = "NONE" },
-        },
-      },
-    },
-  },
-  {
     "LazyVim/LazyVim",
-    opts = { colorscheme = "carbonfox" },
+    opts = { colorscheme = "makima" },
   },
   {
     "j-hui/fidget.nvim",
